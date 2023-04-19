0722c8f901650fa9b0e644db24cf54921de628c4
nomnivore <6979410+nomnivore@users.noreply.github.com>
2023-04-19 02:54:32 -0400

    update types

--- src/botCommand.ts
+++ src/botCommand.ts
@@ -1,6 +1,18 @@
-import { SlashCommandBuilder, CommandInteraction } from "discord.js";
+import {
+  SlashCommandBuilder,
+  CommandInteraction,
+  SlashCommandSubcommandBuilder,
+} from "discord.js";
+
+type SlashCommandBaseBuilder = Omit<
+  SlashCommandBuilder,
+  "addSubcommand" | "addSubcommandGroup"
+>;
 
 export interface BotCommand {
-  meta: SlashCommandBuilder;
+  meta:
+    | SlashCommandBuilder
+    | SlashCommandSubcommandBuilder
+    | SlashCommandBaseBuilder;
   run(interaction: CommandInteraction): Promise<unknown>;
 }
