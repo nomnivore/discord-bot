a1584095f1f22d912d9b5cd5b5546b2b7032299e
nomnivore <6979410+nomnivore@users.noreply.github.com>
2023-04-19 02:55:56 -0400

    add commands

---
+++ src/commands/choose.ts
@@ -0,0 +1,40 @@
+import { SlashCommandBuilder } from "discord.js";
+import { BotCommand } from "../botCommand.js";
+import { z } from "zod";
+
+const command = {
+  meta: new SlashCommandBuilder()
+    .setName("choose")
+    .setDescription("Choose between two options")
+    .addStringOption((option) =>
+      option.setName("option1").setRequired(true).setDescription("First option")
+    )
+    .addStringOption((option) =>
+      option
+        .setName("option2")
+        .setRequired(true)
+        .setDescription("Second option")
+    ),
+
+  async run(interaction) {
+    const options = z
+      .object({
+        option1: z.string().nonempty(),
+        option2: z.string().nonempty(),
+      })
+      .parse({
+        option1: interaction.options.get("option1", true).value,
+        option2: interaction.options.get("option2", true).value,
+      });
+
+    const random = Math.floor(Math.random() * 2);
+
+    const result = random === 0 ? options.option1 : options.option2;
+
+    await interaction.reply({
+      content: `I choose: ${result}!`,
+    });
+  },
+} satisfies BotCommand;
+
+export default command;
---
+++ src/commands/confirm.ts
@@ -0,0 +1,66 @@
+import {
+  ComponentType,
+  SlashCommandBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+} from "discord.js";
+import { BotCommand } from "../botCommand.js";
+
+const command: BotCommand = {
+  meta: new SlashCommandBuilder()
+    .setName("confirm")
+    .setDescription("Confirm a command (proof of concept)"),
+
+  async run(interaction) {
+    const confirmButton = new ButtonBuilder()
+      .setLabel("Confirm")
+      .setCustomId("confirm")
+      .setStyle(ButtonStyle.Success)
+      .setEmoji("✅");
+
+    const cancelButton = new ButtonBuilder()
+      .setLabel("Cancel")
+      .setCustomId("cancel")
+      .setStyle(ButtonStyle.Secondary)
+      .setEmoji("❌");
+    const confirmMsg = await interaction.reply({
+      content: "Please confirm this action",
+      ephemeral: true,
+      components: [
+        new ActionRowBuilder<ButtonBuilder>().addComponents(
+          confirmButton,
+          cancelButton
+        ),
+      ],
+    });
+
+    try {
+      const x = await confirmMsg.awaitMessageComponent({
+        componentType: ComponentType.Button,
+        time: 15000,
+        filter: (i) => i.user.id === interaction.user.id && i.isButton(),
+      });
+
+      if (x.customId === "confirm") {
+        await interaction.editReply({
+          content: "Confirmed!",
+          components: [],
+        });
+      }
+
+      if (x.customId === "cancel") {
+        await interaction.editReply({ content: "Cancelled.", components: [] });
+      }
+    } catch (err: unknown) {
+      if (err instanceof Error && err.message.endsWith("time")) {
+        await interaction.editReply({
+          content: "Cancelled: Timed out.",
+          components: [],
+        });
+      }
+    }
+  },
+};
+
+export default command;
