1e1ff5a6abe7a95a909e21ef9f475ab250c67e7d
nomnivore <6979410+nomnivore@users.noreply.github.com>
2023-04-21 15:24:08 -0400

    add logger module

---
+++ src/util/logger.ts
@@ -0,0 +1,36 @@
+import { Chalk } from "chalk";
+
+export class Logger {
+  private readonly chalk = new Chalk();
+
+  private readonly logLevels = [
+    { label: "debug", color: this.chalk.gray },
+    { label: "info", color: this.chalk.blue },
+    { label: "warn", color: this.chalk.yellow },
+    { label: "error", color: this.chalk.red },
+  ];
+  public readonly logLevel: number;
+
+  constructor(logLevel = 1) {
+    this.logLevel = logLevel;
+  }
+
+  private logFactory(logLevel: number) {
+    return (...args: unknown[]): void => {
+      if (logLevel >= this.logLevel) {
+        const level = this.logLevels.at(logLevel);
+        if (level !== undefined) {
+          console.log(
+            level.color(`[${level.label.toUpperCase()}]`).padEnd(8),
+            ...args
+          );
+        }
+      }
+    };
+  }
+
+  public debug = this.logFactory(0);
+  public info = this.logFactory(1);
+  public warn = this.logFactory(2);
+  public error = this.logFactory(3);
+}
